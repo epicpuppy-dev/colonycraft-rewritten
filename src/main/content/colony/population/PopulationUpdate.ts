a14b71d167de620a8a9a62fd7cbcd6af575b7d01
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2023-10-04 14:21:40 -0700

    Make health affect population growth

--- src/main/content/colony/population/PopulationUpdate.ts
+++ src/main/content/colony/population/PopulationUpdate.ts
@@ -8,24 +8,25 @@ export class PopulationUpdate extends TickingEntity {
 
     public tick (game: ColonyCraft) {
         const population = game.colony.population;
+        const health = game.colony.welfare.healthModifier;
         //0.2% chance of death per day
         if (population.seniors > 10000) {
-            population.seniors -= Math.floor(Math.random() * population.seniors / 1000);
+            population.seniors -= Math.floor(Math.random() * population.seniors / ((1 / 0.002) * (1 / health)));
         } else {
             for (let i = 0; i < population.seniors; i++) {
-                if (Math.random() < 0.002) {
+                if (Math.random() < 0.002 * (1 / health)) {
                     population.seniors--;
                 }
             }
         }
         //0.04% chance for each adult to age into a senior
         if (population.adults > 10000) {
-            const ageup = Math.floor(Math.random() * population.adults / 1250);
+            const ageup = Math.floor(Math.random() * population.adults / ((1 / 0.0004) * (1 / health)));
             population.adults -= ageup;
             population.seniors += ageup;
         } else {
             for (let i = 0; i < population.adults; i++) {
-                if (Math.random() < 0.0004) {
+                if (Math.random() < 0.0004 * (1 / health)) {
                     population.adults--;
                     population.seniors++;
                 }
@@ -33,10 +34,10 @@ export class PopulationUpdate extends TickingEntity {
         }
         //0.04% chance for each adult to immediately die
         if (population.adults > 10000) {
-            population.adults -= Math.floor(Math.random() * population.adults / 1250);
+            population.adults -= Math.floor(Math.random() * population.adults / ((1 / 0.0004) * (1 / health)));
         } else {
             for (let i = 0; i < population.adults; i++) {
-                if (Math.random() < 0.0004) {
+                if (Math.random() < 0.0004 * (1 / health)) {
                     population.adults--;
                 }
             }
@@ -69,11 +70,11 @@ export class PopulationUpdate extends TickingEntity {
         }
         //0.2% chance for each pair of adults to have a baby
         if (population.adults > 10000) {
-            const babies = Math.floor(Math.random() * population.adults / 500);
+            const babies = Math.floor(Math.random() * population.adults / ((1 / 0.001) * health));
             population.babies += babies;
         } else {
             for (let i = 0; i < population.adults / 2; i++) {
-                if (Math.random() < 0.002) {
+                if (Math.random() < 0.002 * health) {
                     population.babies++;
                 }
             }
