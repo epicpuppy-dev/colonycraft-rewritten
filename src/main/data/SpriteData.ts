fc577c9a2b6f29e35f35bf1e9602b83e590f7b9a
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2024-02-02 08:41:56 -0800

    Add new sprites

--- src/main/data/SpriteData.ts
+++ src/main/data/SpriteData.ts
@@ -34,48 +34,19 @@ export class SpriteData {
             "statsSmall": [96, 0, 16, 16],
         });
 
-        renderer.addSheetWithSprites("items", itemsSmall, {
-            "berriesSmall": [0, 0, 16, 16], 
-            "herbsSmall": [16, 0, 16, 16],
-            "breadSmall": [32, 0, 16, 16],
-            "cookedFishSmall": [48, 0, 16, 16],
-            //"cookedMeatSmall": [64, 0, 16, 16],
-            "fishSmall": [80, 0, 16, 16],
-            //"meatSmall": [96, 0, 16, 16],
-            "freshWaterSmall": [112, 0, 16, 16],
-            "muddyWaterSmall": [0, 16, 16, 16],
-            "tool1Small": [16, 16, 16, 16],
-            "tool2Small": [32, 16, 16, 16],
-            //"tool3Small": [48, 16, 16, 16],
-            //"basket1Small": [64, 16, 16, 16],
-            //"bucket1Small": [80, 16, 16, 16],
-            "campfireSmall": [96, 16, 16, 16],
-            //"clothing1Small": [112, 16, 16, 16],
-            "sticksSmall": [0, 32, 16, 16],
-            "rocksSmall": [16, 32, 16, 16],
-            "leavesSmall": [32, 32, 16, 16],
-            "fiberSmall": [48, 32, 16, 16],
-            "hideSmall": [64, 32, 16, 16],
-            //"claySmall": [80, 32, 16, 16],
-            //"mudSmall": [96, 32, 16, 16],
-            //"compositeSmall": [112, 32, 16, 16],
-            //"twineSmall": [0, 48, 16, 16],
-            //"clothSmall": [16, 48, 16, 16],
-            //"logsSmall": [32, 48, 16, 16],
-            //"planksSmall": [48, 48, 16, 16],
-            //"beamsSmall": [64, 48, 16, 16],
-            "wheatSmall": [80, 48, 16, 16],
-            "flourSmall": [96, 48, 16, 16],
-            //"potatoSmall": [112, 48, 16, 16],
-            "stoneSmall": [0, 64, 16, 16],
-            "coalSmall": [16, 64, 16, 16],
-            "copperOreSmall": [32, 64, 16, 16],
-            "tinOreSmall": [48, 64, 16, 16],
-            "ironOreSmall": [64, 64, 16, 16],
-            "zincOreSmall": [80, 64, 16, 16],
-            "siliconOreSmall": [96, 64, 16, 16],
-            "goldOreSmall": [112, 64, 16, 16],
-        });
+        let items: {[key: string]: [number, number, number, number]} = {};
+        const itemList = [
+            "berries", "herbs", "bread", "cookedFish", "cookedMeat", "fish", "meat", 
+            "freshWater", "muddyWater", "tool1", "tool2", "tool3", "basket1", "bucket1",
+            "campfire", "clothing1", "sticks", "rocks", "leaves", "fiber", "hide", "clay",
+            "composite", "twine", "cloth", "logs", "planks", "wheat", "flour", "potato",
+            "stone", "coal", "copperOre", "tinOre", "ironOre", "zincOre", "siliconOre", "goldOre",
+            "bricks", "copper", "tin", "iron", "zinc", "bronze", "steel", "gold"];
+        for (let i = 0; i < itemList.length; i++) {
+            items[itemList[i].toString() + "Small"] = [(i % 8) * 16, Math.floor(i / 8) * 16, 16, 16]
+        }
+
+        renderer.addSheetWithSprites("items", itemsSmall, items);
 
         renderer.addSheetWithSprites("temp", temp, {
             "temp": [0, 0, 32, 32],
